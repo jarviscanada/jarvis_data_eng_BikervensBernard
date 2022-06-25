0fa3da9a07208028e59666ac2fcc59369215d596
bernard676 <bernardbk6@gmail.com>
2022-06-24 23:14:33 -0400

    bts

--- practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
@@ -15,14 +15,8 @@ class HelloWorld {
     bst.insert(new Node( 1));
     bst.insert(new Node( 5));
     bst.insert(new Node( 13));
-    bst.insert(new Node( 3));bst.printTree();
-    bst.delete(new Node( 2));
-    System.out.println("=======================");
-    System.out.println("");
+    bst.insert(new Node( 3));
     bst.printTree();
-    bst.traverse(String.valueOf(BST.Traversal.PRE_ORDER));
-    bst.traverse(String.valueOf(BST.Traversal.POST_ORDER));
-    bst.traverse(String.valueOf(BST.Traversal.IN_ORDER));
-    System.out.println(bst.find(new Node(3)).data);
+
   }
 }
