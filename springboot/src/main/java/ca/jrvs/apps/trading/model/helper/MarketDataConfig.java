70b0a5fc9bd5fa7f37b3c914e97250beebb37d6c
bernard676 <bernardbk6@gmail.com>
2022-08-15 12:38:38 -0400

    backend springboot project

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/helper/MarketDataConfig.java
@@ -0,0 +1,33 @@
+package ca.jrvs.apps.trading.model.helper;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@org.springframework.stereotype.Service
+public class MarketDataConfig {
+    //URI Symbols
+    public static final String QUERY_SYM = "?";
+    public static final String PERCENT = "%";
+    public static final String AMPERSAND = "&";
+    public static final String EQUAL = "=";
+    public static final int HTTP_OK = 200;
+    private String host;
+
+    @Value("${token}")
+    private String token;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
